925364b8327086b786c8005672649c3467cf3b30
CrisMachuca <cristinamachucam@gmail.com>
2024-03-03 23:16:16 +0100

    bugs fixed

--- app.js
+++ app.js
@@ -1,47 +1,46 @@
 // Arrays 
-const pronoun = ['the', 'our'];
-const adj = ['great', 'big'];
-const noun = ['jogger', 'racoon'];
-const extension = ['.com', '.es', '.net', '.us', '.io'];
+const PRONOUN = ['the', 'our'];
+const ADJ = ['great', 'big'];
+const NOUN = ['jogger', 'racoon'];
+const EXTENSION = ['.com', '.es', '.net', '.us', '.io'];
+
+let domainList = [];
 
 // TOTALLY RANDOM DOMAIN NAME
 
-// function for contact all the arrays in a new array and generate a random element of every array
+// function that generates all possible combinations of the 4 given arrays
 
-    function generateDomain() {
-      let domains = [];
-      for (let i = 0; i < pronoun.length; i++) {
-        for (let j = 0; j < adj.length; j++) {
-          for (let k = 0; k < noun.length; k++) {
-            for(let l = 0; l < extension.length; l++) {
-                domains.push(pronoun[i] + adj[j] + noun[k] + extension[l]);
+function generateDomain() {
+        for (let i = 0; i < PRONOUN.length; i++) {
+            for (let j = 0; j < ADJ.length; j++) {
+                for (let k = 0; k < NOUN.length; k++) {
+                    for(let l = 0; l < EXTENSION.length; l++) {
+                        domainList.push(PRONOUN[i] + ADJ[j] + NOUN[k] + EXTENSION[l]);
+                    }
+                }
             }
-          }
         }
-      }
-      let randomIndex = Math.floor(Math.random() * domains.length);
-      // print de value of the new array in the input of HTML called domainInput
-      document.getElementById('domainInput').value = domains[randomIndex];
+      let randomIndex = Math.floor(Math.random() * domainList.length);
+      // print de value of the new array
+      document.getElementById('domainInput').value = domainList[randomIndex];
     }
 
 
 // CUSTOM RANDOM DOMAIN NAME
-
-// function for contact all the arrays in a new array and generate a random element of every array. Includes custom noun.
-
-    function generateCustomDomain() {
-        // custonNoun recives de value typed in the input of HTML called customNounInput deleting whitespaces
-        let customNoun = document.getElementById('customNounInput').value.trim();
-        if (customNoun === '') {
-          alert('Type a word for the noun');
-          return;
-        }
+// function that generates all combinations given 3 arrays and a string
+function generateCustomDomain() {
+    // custonNoun gets the input and deletes spaces
+    let customNoun = document.getElementById('customNounInput').value.trim();
+    if (customNoun === '') {
+        alert('Type a word for the noun');
+        return;
+    }
   
-        let domains = [];
-        for (let i = 0; i < pronoun.length; i++) {
-          for (let j = 0; j < adj.length; j++) {
-            for (let k = 0; k < extension.length; k++) {
-                domains.push(pronoun[i] + adj[j] + customNoun + extension[k]);
+    let domains = [];
+        for (let i = 0; i < PRONOUN.length; i++) {
+          for (let j = 0; j < ADJ.length; j++) {
+            for (let k = 0; k < EXTENSION.length; k++) {
+                domains.push(PRONOUN[i] + ADJ[j] + customNoun + EXTENSION[k]);
             }
           }
         }
@@ -52,33 +51,23 @@ const extension = ['.com', '.es', '.net', '.us', '.io'];
       }
 
 
-// Generates a list of oll the cominations
-    function generateDomainList() {
-      let domains = [];
-      for (let i = 0; i < pronoun.length; i++) {
-        for (let j = 0; j < adj.length; j++) {
-          for (let k = 0; k < noun.length; k++) {
-            for (let l = 0; l < extension.length; l++) {
-              domains.push(pronoun[i] + adj[j] + noun[k] + extension[l]);
-            }
-          }
-        }
-      }
-        return domains;
-}
+
 // Print a list of oll the cominations
-    function printDomainList() {
-      let domainList = generateDomainList();
-      let listContainer = document.getElementById('listas');
-      listContainer.innerHTML = ''; // Clean de div's content
-      let ul = document.createElement('ul');
-      domainList.forEach(function (domain) {
-      let li = document.createElement('li');
-      li.textContent = domain;
-      ul.appendChild(li);
+function printDomainList() {
+        let listContainer = document.getElementById('listas');
+    listContainer.textContent = ''; // Clean de div's content
+    let ul = document.createElement('ul');
+
+    domainList.forEach(function (domain) {
+        let li = document.createElement('li');
+        li.textContent = domain;
+        ul.appendChild(li);
     });
+
   listContainer.appendChild(ul);
 }
 
+
 // Call the function for print de list when page is loaded
-  window.onload = printDomainList;
+generateDomain();
+window.onload = printDomainList;
